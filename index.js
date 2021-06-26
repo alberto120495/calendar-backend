e41d0cf3a5562cb7e1fa6e33ed2bf364ab5e4d12
AlbertoPimentel <albertopimentel04@gmail.com>
2021-06-26 15:01:08 -0500

    backend completo para las notas CRUD

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.static("public"));
 
 //!Rutas
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
 
 //!Escuchar peticiones
 app.listen(process.env.PORT, () => {
